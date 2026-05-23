be60fbba778ec568557fc01937a55baa9aa6336e
AsiaWu_a <wuasia2007@hotmail.com>
2026-05-23 09:54:34 +0900

    data 5/23

---
+++ 2510801122吴亚洲数据结构上机/上机7/queue.c
@@ -0,0 +1,49 @@
+﻿#include "queue.h"
+
+//front 指向的数组中的头元素
+//rear 指向的数组中的为元素的后一位
+
+void InitQueue(SeqQueue* Q)
+{
+	Q->front = Q->rear = 0;
+}
+
+int EnterQueue(SeqQueue* Q, QElemType e)
+{
+	if ((Q->rear + 1) % QueueSize == Q->front)
+		return FALSE;
+	Q->elem[Q->rear] = e;
+	Q->rear = (Q->rear + 1) % QueueSize;
+	return TRUE;
+}
+int DeleteQueue(SeqQueue* Q, QElemType* e)
+{
+	if (Q->rear == Q->front)
+		return FALSE;
+	*e = Q->elem[Q->front];
+	Q->front = (Q->front + 1) % QueueSize;
+	return TRUE;
+}
+int IsEmptyQueue(SeqQueue* Q)
+{
+	if (Q->rear == Q->front) return 1;
+	else return 0;
+}
+int IsFullQueue(SeqQueue* Q)
+{
+	if ((Q->rear + 1) % QueueSize == Q->front) return 1;
+	else return 0;
+}
+
+int GetHead(SeqQueue* Q, QElemType* e)
+{
+	if (IsEmptyQueue(Q)) return FALSE;
+	*e = Q->elem[Q->front];
+	return TRUE;
+}
+int GetTail(SeqQueue* Q, QElemType* e)
+{
+	if (IsEmptyQueue(Q)) return FALSE;
+	*e = Q->elem[(Q->rear - 1 + QueueSize) % QueueSize];
+	return TRUE;
+}
---
+++ 2510801122吴亚洲数据结构上机/上机7/stack.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include<stdio.h>
+#include<stdlib.h>
+
+//#define StackSize 10010
+//typedef int ElemType;
+
+typedef struct BiTNode * BiTree;
+
+#define StackSize 25
+typedef BiTree ElemType;
+#define TRUE 1
+#define FALSE 0
+typedef struct
+{
+	ElemType elem[StackSize];
+	int top;
+}SeqStack;
+
+
+void InitStack(SeqStack* s);
+int IsStackEmpty(SeqStack* s);
+int IsStackFull(SeqStack* s);
+int Push(SeqStack* s, ElemType x);
+int Pop(SeqStack* s, ElemType* e);
+int GetTop(SeqStack* s, ElemType* e);
